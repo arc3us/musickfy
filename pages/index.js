bd44d34a25d69e8764738a96eaae1b947659502a
arc3us <arinjayjain380@gmail.com>
2022-05-01 22:22:32 +0530

    Reinit with JS

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Sidebar from '../components/Sidebar'
+import styles from '../styles/Home.module.css'
+
+const Home = () => {
+  return (
+    <main>
+      <Sidebar />
+    </main>
+  )
+}
+
+export default Home;
